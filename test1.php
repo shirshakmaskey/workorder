987a4c990cee911052827d0ea503cc456a80093d
shirshakmaskey <shirshak.maskey17@gmail.com>
2016-12-28 13:32:21 +0545

    please open the file

--- test1.php
+++ test1.php
@@ -1,2 +1,6 @@
 <?php
+echo "i am having problem here"."can only create from web";
+echo "<br> cannot write from sublime text and  add to folderr";
+echo "typing from bash";
+i am using as a root user
 ?>
